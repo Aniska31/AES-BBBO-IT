eb2b07af8469bcf76aa00ca48a7aedb3ca5c795b
aniska31 <Avstralia08@mail.ru>
2021-03-14 11:01:40 +0300

    prak2

---
+++ prak2/1/1/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace _1
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.radioButton3 = new System.Windows.Forms.RadioButton();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.textBox4 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.groupBox1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.label1.Location = new System.Drawing.Point(8, 81);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(121, 26);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Введите x:";
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(135, 86);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(298, 20);
+      this.textBox2.TabIndex = 7;
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Checked = true;
+      this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.radioButton1.Location = new System.Drawing.Point(6, 37);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(51, 28);
+      this.radioButton1.TabIndex = 8;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "Sh";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.radioButton2.Location = new System.Drawing.Point(6, 64);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(52, 28);
+      this.radioButton2.TabIndex = 9;
+      this.radioButton2.TabStop = true;
+      this.radioButton2.Text = "Ch";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // radioButton3
+      // 
+      this.radioButton3.AutoSize = true;
+      this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.radioButton3.Location = new System.Drawing.Point(6, 98);
+      this.radioButton3.Name = "radioButton3";
+      this.radioButton3.Size = new System.Drawing.Size(62, 28);
+      this.radioButton3.TabIndex = 10;
+      this.radioButton3.TabStop = true;
+      this.radioButton3.Text = "Exp";
+      this.radioButton3.UseVisualStyleBackColor = true;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.radioButton2);
+      this.groupBox1.Controls.Add(this.radioButton3);
+      this.groupBox1.Controls.Add(this.radioButton1);
+      this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.groupBox1.Location = new System.Drawing.Point(443, 22);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(81, 132);
+      this.groupBox1.TabIndex = 11;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "F(x)";
+      // 
+      // textBox4
+      // 
+      this.textBox4.Location = new System.Drawing.Point(13, 184);
+      this.textBox4.Multiline = true;
+      this.textBox4.Name = "textBox4";
+      this.textBox4.Size = new System.Drawing.Size(759, 307);
+      this.textBox4.TabIndex = 12;
+      // 
+      // button1
+      // 
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.button1.Location = new System.Drawing.Point(325, 497);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(131, 52);
+      this.button1.TabIndex = 13;
+      this.button1.Text = "Расчет";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = global::_1.Properties.Resources.uravnenie;
+      this.pictureBox1.Location = new System.Drawing.Point(531, 33);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(241, 121);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+      this.pictureBox1.TabIndex = 14;
+      this.pictureBox1.TabStop = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(784, 561);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox4);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ prak2/1/1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+  
+  public partial class Form1 : Form
+  {
+    public double Function(string _func_name,double _X)
+    {
+      if (_func_name == "Sh") return Math.Sinh(_X);
+      if (_func_name == "Ch") return Math.Cosh(_X);
+      else return Math.Exp(_X);
+    }
+    public  double Solution(string _func_name, double _X)
+    {
+      if((_X>=0)&&(_X<=3))
+          return 2;
+      if (_X > 3) return (0.5 * Function(_func_name, _X));
+        else
+          return (1 / Function(_func_name, _X));
+    }
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      string func = "";
+      if (radioButton1.Checked)
+        func = "Sh";
+      else
+          if ((radioButton2.Checked))
+            func = "Ch";
+          else
+            func = "Exp";
+      if (textBox2.Text == "")
+        {
+        MessageBox.Show("Вы не ввели x.Введите его.");
+        return;
+        }
+      double X = Convert.ToDouble(textBox2.Text);
+      textBox4.Text += Environment.NewLine + "При X:" + X + " и F(x):"+func + " Y:" + Solution(func,X);
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      textBox4.Text += "Результаты работ программы студента Анисимова Е.С.";
+    }
+  }
+}
