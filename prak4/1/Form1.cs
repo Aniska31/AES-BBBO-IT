6a73a73038135f233a2cf8b8c6ce659341ef8bb7
aniska31 <Avstralia08@mail.ru>
2021-04-06 17:38:35 +0300

    add prak4 and task for prak5

---
+++ prak4/1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _1
+{
+  public partial class Form1 : Form
+  {
+    Chart chart;
+    private double XMin = 0.1;
+    private double XMax = 9;
+    private double Step = 0.1;
+    private double[] x;
+    private double[] y;
+    //private double[] y2;
+
+    private void CalcFunction()
+    {
+      // Количество точек графика
+      int count = (int)Math.Ceiling((XMax - XMin) / Step) + 1;
+      // Создаём массивы нужных размеров
+      x = new double[count];
+      y = new double[count];
+      //y2 = new double[count];
+      // Расчитываем точки для графиков функции
+      for (int i = 0; i < count; i++)
+      {
+        // Вычисляем значение X
+        x[i] = XMin + Step * i;
+        // Вычисляем значение функций в точке X
+        y[i] = (Math.Sqrt(3 + Math.Log(x[i]) + 15 - x[i])) / (1 + Math.Sin((2 + x[i] * x[i]) / (1 + x[i])));
+        //y2[i] = Math.Cos(x[i]);
+      }
+    }
+    private void CreateChart()
+    {
+      // Создаёмновыйэлементуправления Chart
+      chart = new Chart();
+      // Помещаем его на форму
+      chart.Parent = this;
+      // Задаём размеры элемента
+      chart.SetBounds(10, 10, ClientSize.Width - 20,
+      ClientSize.Height - 20);
+      // Создаём новую область для построения графика
+      ChartArea area = new ChartArea();
+      // Даём ей имя (чтобы потом добавлять графики)
+      area.Name = "myGraph";
+      // Задаём левую и правую границы оси X
+      area.AxisX.Minimum = XMin;
+      area.AxisX.Maximum = XMax;
+      // Определяемшагсетки
+      area.AxisX.MajorGrid.Interval = Step;
+      // Добавляем область в диаграмму
+      chart.ChartAreas.Add(area);
+
+      // Создаём объект для первого графика
+      Series series1 = new Series();
+      // Ссылаемся на область для построения графика
+      series1.ChartArea = "myGraph";
+      // Задаём тип графика - сплайны
+      series1.ChartType = SeriesChartType.Spline;
+      // Указываем ширину линии графика
+      series1.BorderWidth = 3;
+      // Название графика для отображения в легенде
+      series1.LegendText = "11 вариант";
+      // Добавляем в список графиков диаграммы
+      chart.Series.Add(series1);
+     /* // Аналогичные действия для второго графика
+      Series series2 = new Series();
+      series2.ChartArea = "myGraph";
+      series2.ChartType = SeriesChartType.Spline;
+      series2.BorderWidth = 3;
+      series2.LegendText = "cos(x)";
+      chart.Series.Add(series2);*/
+
+      // Создаёмлегенду, котораябудетпоказыватьназвания
+      Legend legend = new Legend();
+      chart.Legends.Add(legend);
+    }
+
+  public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      // Создаём элемент управления
+      CreateChart();
+      // Расчитываем значения точек графиков функций
+      CalcFunction();
+      // Добавляем вычисленные значения в графики
+      chart.Series[0].Points.DataBindXY(x, y);
+      //chart.Series[1].Points.DataBindXY(x, y2);
+    }
+  }
+}
---
+++ prak4/2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+
+    int change_1= 0;
+    int change_2 = 0;
+    int color = 0;
+    Random rnd = new Random();
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+        Graphics g = e.Graphics; // Объявляется объект Graphics. Далее вставляется код рисования
+        Pen pen;
+        if (color%2==0)
+        {
+          g.Clear(Color.DeepSkyBlue);
+          pen = new Pen(Brushes.Black);
+        }
+        else
+        {
+          g.Clear(Color.Black);
+          pen = new Pen(Brushes.DeepSkyBlue);
+         }
+        Graphics circle_main = this.CreateGraphics();
+        Graphics circle_left = this.CreateGraphics();
+        Graphics circle_right = this.CreateGraphics();
+        Graphics trinagle_up = this.CreateGraphics();
+        Graphics trinagle_left = this.CreateGraphics();
+        Graphics trinagle_right = this.CreateGraphics();
+        Graphics cross_left = this.CreateGraphics();
+        Graphics cross_right = this.CreateGraphics();
+        circle_main.DrawEllipse(pen, 350 + change_1, 250 + change_2, 70, 70);//нос
+        circle_left.DrawEllipse(pen, 280 + change_1, 300 + change_2, 35, 35);//пропеллер левый
+        circle_right.DrawEllipse(pen, 450 + change_1, 300 + change_2, 35, 35);//пропеллер правый
+        Point[] points = new Point[3];
+        points[0].X = 150 + change_1; points[0].Y = 285 + change_2;
+        points[1].X = 355 + change_1; points[1].Y = 265 + change_2;
+        points[2].X = 355 + change_1; points[2].Y = 305 + change_2;
+        trinagle_left.DrawPolygon(pen, points);//левое крыло
+        points[0].X = 415 + change_1; points[0].Y = 305 + change_2;
+        points[1].X = 415 + change_1; points[1].Y = 265 + change_2;
+        points[2].X = 620 + change_1; points[2].Y = 285 + change_2;
+        trinagle_right.DrawPolygon(pen, points);//правое крыло
+        points[0].X = 335 + change_1; points[0].Y = 250 + change_2;
+        points[1].X = 435 + change_1; points[1].Y = 250 + change_2;
+        points[2].X = 385 + change_1; points[2].Y = 200 + change_2;
+        trinagle_up.DrawPolygon(pen, points);//вверх
+        cross_left.DrawLine(pen, 280 + change_1, 300 + change_2, 315 + change_1, 335 + change_2);// крест левый1
+        cross_left.DrawLine(pen, 280 + change_1, 335 + change_2, 315 + change_1, 300 + change_2); // крест левый2
+        cross_right.DrawLine(pen, 450 + change_1, 300 + change_2, 485 + change_1, 335 + change_2);// крест правый1
+        cross_right.DrawLine(pen, 450 + change_1, 335 + change_2, 485 + change_1, 300 + change_2);// крест правый1
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      change_1 = rnd.Next(-100, 100);
+      change_2 = rnd.Next(-100, 100);
+      color++;
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      change_1 = rnd.Next(-100, 100);
+      change_2 = rnd.Next(-100, 100);
+      color++;
+    }
+  }
+}
